cf5cfdd3ee3ef3c9c5c3ca18d12862eefe1dc60b
Sudipto <sudiptog81@gmail.com>
2018-12-14 09:58:36 +0530

    fixed search

--- src/components/tracks/Search.jsx
+++ src/components/tracks/Search.jsx
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 import { Consumer } from "./../../context";
 import axios from "axios";
 class Search extends Component {
-  state = { 
+  state = {
     trackTitle: "",
     artistName: "",
     pageSize: 10,
     findSongs: "Find Songs"
-   };
+  };
   onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
@@ -17,8 +17,10 @@ class Search extends Component {
       .get(
         `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?f_has_lyrics=1&q_track=${
           this.state.trackTitle
-        }&q_artist=${this.state.artistName}&page_size=${this.state.pageSize}&page=1&s_track_rating=desc&apikey=${process.env
-          .REACT_APP_MM_KEY || "Your MusixMatch API Key"}`
+        }&q_artist=${this.state.artistName}&page_size=${
+          this.state.pageSize
+        }&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY ||
+          "Your MusixMatch API Key"}`
       )
       .then(res => {
         dispatch({
@@ -26,8 +28,7 @@ class Search extends Component {
           payload: res.data.message.body.track_list
         });
         const size = this.state.pageSize;
-        this.setState({ trackTitle: "", artistName: "", pageSize: size + 9, findSongs: "Find More Songs"});
-
+        this.setState({ pageSize: size + 10, findSongs: "Find More Songs" });
       })
       .catch(err => console.log(err));
   };
@@ -36,20 +37,40 @@ class Search extends Component {
       <Consumer>
         {value => {
           const { dispatch } = value;
-          return <div className="mb-4">
-              <h2 className="display-4 text-center">
-                get lyrics for any song
-              </h2>
-              <form className="mt-4" onSubmit={this.findTrack.bind(this, dispatch)}>
+          return (
+            <div className="mb-4">
+              <h2 className="display-4 text-center">get lyrics for any song</h2>
+              <form
+                className="mt-4"
+                onSubmit={this.findTrack.bind(this, dispatch)}
+              >
                 <div className="form-group">
-                  <input type="text" className="form-control form-control-lg text-center mb-2" placeholder="Search by Title" name="trackTitle" onChange={this.onChange} value={this.state.trackTitle} />
-                  <input type="text" className="form-control form-control-lg text-center mt-2" placeholder="Search by Artist" name="artistName" onChange={this.onChange} value={this.state.artistName} />
+                  <input
+                    type="text"
+                    className="form-control form-control-lg text-center mb-2"
+                    placeholder="Search by Title"
+                    name="trackTitle"
+                    onChange={this.onChange}
+                    value={this.state.trackTitle}
+                  />
+                  <input
+                    type="text"
+                    className="form-control form-control-lg text-center mt-2"
+                    placeholder="Search by Artist"
+                    name="artistName"
+                    onChange={this.onChange}
+                    value={this.state.artistName}
+                  />
                 </div>
-                <button className="btn btn-dark btn-lg btn-block mb-4" type="submit">
+                <button
+                  className="btn btn-dark btn-lg btn-block mb-4"
+                  type="submit"
+                >
                   {this.state.findSongs}
                 </button>
               </form>
-            </div>;
+            </div>
+          );
         }}
       </Consumer>
     );
